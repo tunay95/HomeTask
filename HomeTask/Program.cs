6318c590353ace7fa485e4eebcea995012039bbc
TUNAY <TUNAY@DESKTOP-0HF9IVP>
2023-09-27 09:59:10 +0400

    Add project files.

---
+++ HomeTask/Program.cs
@@ -0,0 +1,83 @@
+﻿//using System;
+
+
+//namespace HomeTask
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //Task 1
+//            Console.WriteLine("Input The Numbers : ");
+//            int a = Convert.ToInt32(Console.ReadLine());
+//            int b = Convert.ToInt32(Console.ReadLine());
+//            int c = Convert.ToInt32(Console.ReadLine());
+//            int sum = 0;
+//            if (a > 0 && b > 0 && c > 0)
+//            {
+
+
+//                if (a % 5 == 0)
+//                {
+//                    sum += a;
+//                }
+
+//                if (b % 5 == 0)
+//                {
+//                    sum += b;
+//                }
+
+
+//                if (c % 5 == 0)
+//                {
+//                    sum += c;
+//                }
+
+//            }
+//            else 
+//            {
+//                Console.WriteLine("Enter More Than 0!! ");
+//            }
+//            Console.WriteLine("the sum = " + sum);
+//            Console.ReadKey();
+//        }
+
+//     }
+}
+
+
+
+
+
+
+//                      //Task 2
+
+//            Console.WriteLine("Enter your number:");
+//            int number = Convert.ToInt32(Console.ReadLine());
+//            int a = number / 100;
+//            int b = number / 10 % 10;
+//            int c = number % 10;
+//            int sum = a + b + c;
+//            if (a == 0)
+//            {
+//                Console.WriteLine("The First Digit cannot be  0! ");
+//            }
+//            else
+//            {
+
+//                Console.WriteLine("The sum is : " + sum);
+//            }
+
+
+
+
+
+//            Console.ReadKey();
+//        }
+//    }
+//}
+
+
+
+
+
